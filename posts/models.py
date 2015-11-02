7ace69b5643c73f9bdd07ab37cd639987a5edfd6
Robin Berghuijs <robin@robinberghuijs.com>
2015-11-02 10:06:19 +0100

    passing unit test, set up links template

--- posts/models.py
+++ posts/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 # Create your models here.
-class Post(models.Model):
+class Link(models.Model):
     title = models.CharField(max_length=140)
     url = models.URLField()
 
--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from .models import Link
 
 # Create your views here.
+def all_links(request):
+    links = Link.objects.all()
+    return render(request, 'posts/all_links.html', {'links': links})
