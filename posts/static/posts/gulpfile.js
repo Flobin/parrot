dc625a847799b59ea5f5b784bed5fe0da4dee673
Robin Berghuijs <robin@robinberghuijs.com>
2015-12-01 00:02:25 +0100

    set up static files, gulp, styles, logo, fix templates

---
+++ posts/static/posts/gulpfile.js
@@ -0,0 +1,65 @@
+var gulp = require('gulp'),
+    sass = require('gulp-sass'),
+    autoprefixer = require('gulp-autoprefixer'),
+    minifycss = require('gulp-minify-css'),
+    jshint = require('gulp-jshint'),
+    uglify = require('gulp-uglify'),
+    imagemin = require('gulp-imagemin'),
+    rename = require('gulp-rename'),
+    concat = require('gulp-concat'),
+    notify = require('gulp-notify'),
+    cache = require('gulp-cache'),
+    livereload = require('gulp-livereload'),
+    del = require('del');
+
+gulp.task('styles', function() {
+  return gulp.src('src/styles/main.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(autoprefixer('last 2 version'))
+    .pipe(gulp.dest('build/css'))
+    .pipe(rename({suffix: '.min'}))
+    .pipe(minifycss())
+    .pipe(gulp.dest('build/css'))
+    .pipe(notify({ message: 'Styles task complete' }));
+});
+
+gulp.task('scripts', function() {
+  return gulp.src('src/scripts/**/*.js')
+    .pipe(jshint('.jshintrc'))
+    .pipe(jshint.reporter('default'))
+    .pipe(concat('main.js'))
+    .pipe(gulp.dest('build/js'))
+    .pipe(rename({suffix: '.min'}))
+    .pipe(uglify())
+    .pipe(gulp.dest('build/js'))
+    .pipe(notify({ message: 'Scripts task complete' }));
+});
+
+gulp.task('images', function() {
+  return gulp.src('src/images/**/*')
+    .pipe(cache(imagemin({ optimizationLevel: 5, progressive: true, interlaced: true })))
+    .pipe(gulp.dest('build/img'))
+    .pipe(notify({ message: 'Images task complete' }));
+});
+
+gulp.task('clean', function() {
+    return del(['build/css', 'build/js', 'build/img']);
+});
+
+gulp.task('default', ['clean'], function() {
+    gulp.start('styles', 'scripts', 'images');
+});
+
+gulp.task('watch', function() {
+  // Watch .scss files
+  gulp.watch('src/styles/**/*.scss', ['styles']);
+  // Watch .js files
+  gulp.watch('src/scripts/**/*.js', ['scripts']);
+  // Watch image files
+  gulp.watch('src/images/**/*', ['images']);
+  // Create LiveReload server
+  livereload.listen();
+  // Watch any files in dist/, reload on change
+  gulp.watch(['build/**']).on('change', livereload.changed);
+
+});
